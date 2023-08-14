463ab451de26634e415ee72a4ee12f39f611b8a9
Dorian <dorianoriginaldesings@gmail.com>
2023-08-15 01:19:40 +0200

    Bad solution for course

--- js/scripts.js
+++ js/scripts.js
@@ -3,32 +3,109 @@ const lengthTextElement = document.getElementById('length-text');
 const rangeElement = document.getElementById('range');
 const buttonGenerateElement = document.getElementById('generate-password');
 
-const allowedCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890!@#$%^&*()_+-={}[]:;<>,.?/';
+const uppercaseInputElement = document.getElementById('uppercase-input');
+const lowercaseInputElement = document.getElementById('lowercase-input');
+const numbersInputElement = document.getElementById('numbers-input');
+const symbolsInputElement = document.getElementById('symbols-input');
 
-let passwordLength = 16;
+/* 
+id a cada checkbox
+un if por cada checkbox
+------------------------
+*/
 
-const generateRandomNumber = () => {
-  return Math.floor(Math.random() * allowedCharacters.length);
-};
+const lowercaseCharacters = 'abcdefghijklmnñopqrstuvwxyz';
+const uppercasCharacters = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZ';
+const numbersCharacters = '1234567890';
+const symbolsCharacters = '+-.,!"·$%&/()=?{}';
 
-const generatePassword = () => {
-  let newPassword = '';
-  for (let i = 0; i < passwordLength; i++) {
-    const randomNumber = generateRandomNumber();
-    newPassword += allowedCharacters[randomNumber];
-  }
-  return newPassword;
-};
+let allowedCharacters = '';
 
-const printPassword = () => {
-  passwordElement.value = generatePassword();
-};
+// const allowedCharacters = 'abcdefghijklmnñopqrstuvwxyzABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890+-.,!"·$%&/()=?{}';
 
-const setPasswordLength = event => {
+let passwordLength = rangeElement.value;
+
+lengthTextElement.textContent = passwordLength;
+
+const setPasswordLenght = event => {
   passwordLength = event.target.value;
   lengthTextElement.textContent = passwordLength;
 };
 
-rangeElement.addEventListener('input', setPasswordLength);
+const printPassword = password => {
+  passwordElement.value = password;
+};
 
-buttonGenerateElement.addEventListener('click', printPassword);
+const setDisabledButton = () => {
+  if (allowedCharacters.length) buttonGenerateElement.disabled = false;
+  else buttonGenerateElement.disabled = true;
+};
+
+const checkPasswordOptions = () => {
+  allowedCharacters = '';
+  if (lowercaseInputElement.checked) {
+    allowedCharacters += lowercaseCharacters;
+  }
+  if (uppercaseInputElement.checked) {
+    allowedCharacters += uppercasCharacters;
+  }
+  if (numbersInputElement.checked) {
+    allowedCharacters += numbersCharacters;
+  }
+  if (symbolsInputElement.checked) {
+    allowedCharacters += symbolsCharacters;
+  }
+
+  setDisabledButton();
+};
+
+const generatePassword = () => {
+  let newPassword = '';
+  let newPasswordLength = passwordLength;
+
+  if (lowercaseInputElement.checked) {
+    const randomNumber = Math.floor(Math.random() * lowercaseCharacters.length);
+    newPassword += lowercaseCharacters.charAt(randomNumber);
+    newPasswordLength--;
+  }
+
+  if (uppercaseInputElement.checked) {
+    const randomNumber = Math.floor(Math.random() * uppercasCharacters.length);
+    newPassword += uppercasCharacters.charAt(randomNumber);
+    newPasswordLength--;
+  }
+
+  if (numbersInputElement.checked) {
+    const randomNumber = Math.floor(Math.random() * numbersCharacters.length);
+    newPassword += numbersCharacters.charAt(randomNumber);
+    newPasswordLength--;
+  }
+
+  if (symbolsInputElement.checked) {
+    const randomNumber = Math.floor(Math.random() * symbolsCharacters.length);
+    newPassword += symbolsCharacters.charAt(randomNumber);
+    newPasswordLength--;
+  }
+
+  for (let index = 0; index < newPasswordLength; index++) {
+    const randomNumber = Math.floor(Math.random() * allowedCharacters.length);
+    const randomCharacter = allowedCharacters.charAt(randomNumber);
+    newPassword += randomCharacter;
+  }
+
+  newPassword = newPassword
+    .split('')
+    .sort(() => Math.random() - 0.5)
+    .join('');
+
+  printPassword(newPassword);
+};
+
+rangeElement.addEventListener('input', setPasswordLenght);
+
+buttonGenerateElement.addEventListener('click', generatePassword);
+
+uppercaseInputElement.addEventListener('click', checkPasswordOptions);
+lowercaseInputElement.addEventListener('click', checkPasswordOptions);
+numbersInputElement.addEventListener('click', checkPasswordOptions);
+symbolsInputElement.addEventListener('click', checkPasswordOptions);
